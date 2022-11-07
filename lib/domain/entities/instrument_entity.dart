effe17249e0b35af6af2b853c89dfc5227863de7
Vinicius Gregorio <vinicius.squadp@genial.com.vc>
2022-11-07 17:43:13 -0300

    feat: add instrument entity

---
+++ lib/domain/entities/instrument_entity.dart
@@ -0,0 +1,11 @@
+class InstrumentEntity {
+  String instrumentId;
+  String symbol;
+  String sortIndex;
+
+  InstrumentEntity({
+    this.instrumentId,
+    this.symbol,
+    this.sortIndex,
+  });
+}
